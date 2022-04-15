30516ba64b7ee41c8684943d876839ecc30a36a4
Murielcibra <muriel_cibra@hotmail.com>
2022-04-15 21:10:10 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      Weather App
-      <footer>This project was coded by Muriel Cishek and is </footer>
-      <a
-        href="https://github.com/Murielcibra/react-tempo-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        open-sourced on GitHub{" "}
-      </a>
+    <div class="container">
+      <div className="App">
+        Weather App
+        <footer>This project was coded by Muriel Cishek and is </footer>
+        <a
+          href="https://github.com/Murielcibra/react-tempo-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub{" "}
+        </a>
+      </div>
     </div>
   );
 }
