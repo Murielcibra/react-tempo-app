7e4c34df06dc5cca207851adba5f7cbf5a21156d
Murielcibra <muriel_cibra@hotmail.com>
2022-04-15 20:30:13 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>This project was coded by Muriel Cishek and is </footer>
+      <a
+        href="https://github.com/Murielcibra/react-tempo-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open-sourced on GitHub{" "}
+      </a>
+    </div>
+  );
 }
