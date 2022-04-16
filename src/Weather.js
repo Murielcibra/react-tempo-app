5d85babf9f910a30359488464ac7e076a4a71c8d
Murielcibra <muriel_cibra@hotmail.com>
2022-04-16 23:56:24 +0200

    axios installed

--- src/Weather.js
+++ src/Weather.js
@@ -1,87 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <div className="current-info">
-          <div className="weather-app">
-            <form id="search-form" className="mb-3">
-              <div className="row">
-                <div className="col-9">
-                  <input
-                    type="search"
-                    placeholder="Type a city.."
-                    className="form-control"
-                    id="city-input"
-                    autocomplete="off"
-                  />
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    setWeatherData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl:https://ssl.gstatic.com/onebox/weather/64/sunny.png,
+      city: response.data.name,
+    });
+    setReady(true);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="current-info">
+            <div className="weather-app">
+              <form id="search-form" className="mb-3">
+                <div className="row">
+                  <div className="col-9">
+                    <input
+                      type="search"
+                      placeholder="Type a city.."
+                      className="form-control"
+                      id="city-input"
+                      autocomplete="off"
+                    />
+                  </div>
+                  <div className="col-3">
+                    <input
+                      type="submit"
+                      value="Search"
+                      className="btn btn-primary w-100"
+                    />
+                  </div>
                 </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-primary w-100"
-                  />
-                </div>
+              </form>
+            </div>
+            <div className="date-container">
+              <div className="city" id="city">
+                {weatherData.city}
               </div>
-            </form>
-          </div>
-          <div className="date-container">
-            <div className="city" id="city">
-              San Francisco
-            </div>
-            <div className="date">
-              Last updated: <span id="date"></span>
-            </div>
-            <ul>
-              <li id="description">Clear</li>
-            </ul>
-            <div className="future-forecast">
-              <div className="today" id="current-temp">
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                  alt="weatherIcon"
-                  className="w-icon"
-                  id="icon"
-                />
+              <div className="date">
+                Last updated: <span id="date"></span>
+              </div>
+              <ul>
+                <li id="description">{weatherData.description}</li>
+              </ul>
+              <div className="future-forecast">
+                <div className="today" id="current-temp">
+                  <img
+                    src={weatherData.iconUrl}
+                    alt={weatherData.description}
+                    className="w-icon"
+                    id="icon"
+                  />
 
-                <div className="others">
-                  <div className="day"></div>
+                  <div className="others">
+                    <div className="day"></div>
 
-                  <div className="temp" id="temperature">
-                    72 <span>°</span>
-                  </div>
-                  <div className="units" id="units">
-                    <a href="#" id="celsius-link" className="active">
-                      °C |
-                    </a>
-                    <a href="#" id="fahrenheit-link">
-                      {" "}
-                      °F
-                    </a>
-                  </div>
+                    <div className="temp" id="temperature">
+                      {Math.round(weatherData.temperature)}
+                      <span>°</span>
+                    </div>
+                    <div className="units" id="units">
+                      <a href="#" id="celsius-link" className="active">
+                        °C |
+                      </a>
+                      <a href="#" id="fahrenheit-link">
+                        {" "}
+                        °F
+                      </a>
+                    </div>
 
-                  <div className="humidity">
-                    Humidity:<span id="humidity">75%</span>
-                  </div>
-                  <div>
-                    Windspeed:<span id="windspeed">2km</span>
+                    <div className="humidity">
+                      Humidity:<span id="humidity">{weatherData.humidity}</span>
+                    </div>
+                    <div>
+                      Windspeed:<span id="windspeed">{weatherData.wind}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="weather-forecast" id="weather-forecast"></div>
+                <div className="weather-forecast" id="weather-forecast"></div>
+              </div>
             </div>
+            <img
+              className="motion-image"
+              id="motion-image"
+              src="image/rain.gif"
+            />{" "}
+            <footer>This project was coded by Muriel Cishek and is </footer>
+            <a
+              href="https://github.com/Murielcibra/react-tempo-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub{" "}
+            </a>{" "}
           </div>
-          <img
-            className="motion-image"
-            id="motion-image"
-            src="image/rain.gif"
-          />
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "f6f001d26151b94d121b17eb30bad8c0";
+    let city = "Dominican Republic";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
